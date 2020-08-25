24be6a48fa3045eb71ff0fbe73452be4c81a965c
sseela <s.sabelli@bstore.capgemini.com>
2020-08-25 09:41:39 +0530

    #Test commit on 25-08-2020

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,58 @@
+
+public class DoublyLinkedList {
+
+	private int head;
+	private int tail;
+	private int length;
+	
+	class Node {
+		private int previousValue;
+		private int nextValue;
+		private int data;
+		public int getPreviousValue() {
+			return previousValue;
+		}
+		public void setPreviousValue(int previousValue) {
+			this.previousValue = previousValue;
+		}
+		public int getNextValue() {
+			return nextValue;
+		}
+		public void setNextValue(int nextValue) {
+			this.nextValue = nextValue;
+		}
+		public int getData() {
+			return data;
+		}
+		public void setData(int data) {
+			this.data = data;
+		}
+		
+	}
+
+	public int getHead() {
+		return head;
+	}
+
+	public void setHead(int head) {
+		this.head = head;
+	}
+
+	public int getTail() {
+		return tail;
+	}
+
+	public void setTail(int tail) {
+		this.tail = tail;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
+}
---
+++ src/TestDoublyLinkedList.java
@@ -0,0 +1,14 @@
+
+public class TestDoublyLinkedList {
+
+	private String name;
+	private static String age;
+	public static void main(String[] args) {
+		DoublyLinkedList list = new DoublyLinkedList();
+		list.setHead(0);
+		list.setTail(10);
+		list.setLength(11);
+		
+		
+	}
+}
